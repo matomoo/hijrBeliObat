c8d7f5104f2079db37bc4359f725573c9587d692
zyxgo <me.rahmat.hidayat@gmail.com>
2019-02-05 21:21:57 +0800

    kitt01

--- android/app/src/main/java/com/hijrbeliobat/MainActivity.java
+++ android/app/src/main/java/com/hijrbeliobat/MainActivity.java
@@ -1,4 +1,4 @@
-package com.hijrhec;
+package com.hijrbeliobat;
 
 import android.os.Bundle; // here
 import com.facebook.react.ReactActivity;
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "hijrHec";
+        return "hijrbeliobat";
     }
 
     @Override
--- index.js
+++ index.js
@@ -10,4 +10,4 @@ YellowBox.ignoreWarnings([
   'Module RCTImageLoader',
 ]);
 
-AppRegistry.registerComponent('hijrHec', () => App);
+AppRegistry.registerComponent('hijrbeliobat', () => App);
